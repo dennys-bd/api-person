aa81593bdcc94a7e2670414abce3abebe8ce6a57
Dennys Azevedo <dennys.bd@gmail.com>
2019-02-11 21:28:46 -0300

    api subdomain for inloco graphql talk

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require 'api_version_constraint'
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'personapiinloco' }, path: '/' do
     namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1) do
       resources :persons, param: :uuid, only: [:show, :index, :create, :update]
     end
